f3b41769a672d1ebb51fecca8a3b538c49d6ae3d
ritik.think@gmail.com <ritik.think@gmail.com>
2024-01-03 21:50:53 +0530

    server code updated

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// Import required modules
-import express from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import { configDotenv } from "dotenv";
+// import statements
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
 
 // Load environment variables from a .env file
-configDotenv();
+dotenv.config();
 
 // Create an Express application
 const app = express();
@@ -15,11 +16,17 @@ app.use(bodyParser.json()); // Parse JSON requests
 app.unsubscribe(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded requests
 app.use(morgan("dev")); // Logging middleware
 
-// Basic route
-app.get("/", (req, res) => {
-  console.log(`Request found from ${req.url}`);
-  res.send("Hello world !!");
+// custom middleware
+app.use((req, res, next) => {
+    console.log(`Request URL received for: ${req.url}`);
+    next();
 });
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
 
 // Error middleware for handling 404 errors
 app.use((req, res, next) => {
@@ -41,5 +48,5 @@ app.use((error, req, res, next) => {
 // Start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server started at ${port}`);
+  console.log(`Server started at ${port} 🚀💻`);
 });
